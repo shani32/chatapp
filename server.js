9db8df3228ba9698c4e84371cd4e6bc793464685
shani barak <shanibrk@gmail.com>
2022-04-18 14:05:08 +0300

    server and connect socket

---
+++ server.js
@@ -0,0 +1,20 @@
+const express= require('express')
+const path= require('path')
+const http= require('http')
+const socketio=require('socket.io')
+
+const app= express()
+const server= http.createServer(app)
+const io=socketio(server)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+//new connection
+io.on('connection',socket=>{
+    console.log('new connection')
+})
+
+const PORT= 3000 || process.env.PORT;
+server.listen(PORT, ()=>{
+   console.log(`server is running on port ${PORT}`) 
+})
